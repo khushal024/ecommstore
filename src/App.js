f49eda210d944ef232707ea188cfbae4968d7856
khushal024 <104437910+khushal024@users.noreply.github.com>
2022-07-28 11:43:50 +0530

    new bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.scss';  
-
+import {Heeader} from "./components/header.js"
 function App() {
   return (
     <div className="App">
-
+      <div className="dddd"><Heeader /> </div>
     </div>
   );
 }
