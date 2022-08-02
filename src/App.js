a7258e6737b22bc571df611687212e47c7c95663
khushal024 <khushalwebroot@gmail.com>
2022-08-02 22:48:04 +0530

    just for new code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.scss';  
+import './App.scss'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/js/bootstrap.js";
 
+
+import {Header} from "./components/header"
 function App() {
   return (
-    <div className="App">
-
+    <div className="App"> 
+      <Header />
+<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
